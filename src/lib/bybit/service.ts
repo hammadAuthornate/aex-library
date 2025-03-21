753743e4594b70cf11e13ef7cf4a311144244084
hammadAuthornate <156046487+hammadAuthornate@users.noreply.github.com>
2025-03-21 09:11:05 +0500

    added order POST functions for bybit

--- src/lib/bybit/service.ts
+++ src/lib/bybit/service.ts
@@ -1,4 +1,10 @@
-import { OrderbookResponseV5, RestClientV5, TickerSpotV5 } from "bybit-api";
+import {
+  AccountOrderV5,
+  OrderbookResponseV5,
+  OrderResultV5,
+  RestClientV5,
+  TickerSpotV5,
+} from "bybit-api";
 
 const client = new RestClientV5({ testnet: true });
 
@@ -18,11 +24,60 @@ export class ByBitService {
     return response.result.list?.at(0);
   }
 
-  static async fetchSymbolOrderBook(symbol: string): Promise<OrderbookResponseV5> {
+  static async fetchSymbolOrderBook(
+    symbol: string
+  ): Promise<OrderbookResponseV5> {
     const response = await client.getOrderbook({
       category: "spot",
       symbol: symbol,
     });
     return response.result;
+  }
+
+  static async placeOrder(symbol: string): Promise<OrderResultV5> {
+    const response = await client.submitOrder({
+      category: "spot",
+      symbol: symbol,
+      side: "Buy",
+      orderType: "Market",
+      qty: "1",
+    });
+    return response.result;
+  }
+
+  static async amendOrder(
+    symbol: string,
+    orderId: string
+  ): Promise<OrderResultV5> {
+    const response = await client.amendOrder({
+      category: "spot",
+      symbol: symbol,
+      orderId: orderId,
+    });
+    return response.result;
+  }
+
+  static async cancelOrder(
+    symbol: string,
+    orderId: string
+  ): Promise<OrderResultV5> {
+    const response = await client.cancelOrder({
+      category: "spot",
+      symbol: symbol,
+      orderId: orderId,
+    });
+    return response.result;
+  }
+
+  static async getOrderStatus(
+    symbol: string,
+    orderId: string
+  ): Promise<AccountOrderV5 | undefined> {
+    const response = await client.getActiveOrders({
+      category: "spot",
+      symbol: symbol,
+      orderId: orderId,
+    });
+    return response.result.list?.at(0);
   }
 }
