b963eae5de40dcbaf3f5b9364730bbc2e7cc9ade
Huzaifa <huzaifa@authornate.com>
2025-03-24 12:04:58 +0500

    Base Url Added

--- src/components/Mexc/MexSymbolList.tsx
+++ src/components/Mexc/MexSymbolList.tsx
@@ -5,12 +5,12 @@ import MexSymbolDetails from "./MexSymbolDetails";
 const MexSymbolList: FC = () => {
   const [allSymbols, setAllSymbols] = useState<any>([]);
   const [selectedSymbol, setSelectedSymbol] = useState<string | null>(null);
-
+  const baseUrl = "http://localhost:5000";
   useEffect(() => {
     const fetchSymbols = async () => {
       try {
-        const response = await axios.get("http://localhost:5000/api/symbols");
-        setAllSymbols(response.data); 
+        const response = await axios.get(`${baseUrl}/api/symbols`);
+        setAllSymbols(response.data);
       } catch (error) {
         console.error("Error fetching symbols:", error);
       }
@@ -22,11 +22,14 @@ const MexSymbolList: FC = () => {
     <div>
       <h2>MEXC Spot Trading Symbols</h2>
       <ul style={{ maxHeight: "450px", overflowY: "scroll" }}>
-        {allSymbols?.data?.map((symbol:string) => (
+        {allSymbols?.data?.map((symbol: string) => (
           <li
             key={symbol}
-            onClick={() => setSelectedSymbol(symbol)} 
-            style={{ cursor: "pointer", fontWeight: selectedSymbol === symbol ? "bold" : "normal" }}
+            onClick={() => setSelectedSymbol(symbol)}
+            style={{
+              cursor: "pointer",
+              fontWeight: selectedSymbol === symbol ? "bold" : "normal",
+            }}
           >
             {symbol}
           </li>
